37996633f2973af8ca3d58438fe32ecb29839161
shaalx <shaalx@yahoo.com.sg>
2016-02-22 16:47:40 +0800

    cdln

---
+++ cdln/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"github.com/everfore/exc"
+	// "github.com/shaalx/goutils"
+	"flag"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
+var (
+	write   = false
+	install = false
+)
+
+func init() {
+	flag.BoolVar(&write, "w", false, "-b [true] : git@github.com or git://github.com")
+	flag.BoolVar(&install, "i", false, "-i [true] : go install")
+}
+
+func main() {
+	flag.Parse()
+	var input, user, repo, branch, input_1 /*,target*/ string
+	tips := "[user/]repo[:branch]  > $"
+	fmt.Print(tips)
+	fmt.Scanf("%s", &input)
+
+	start := time.Now()
+	if strings.Contains(input, "/") {
+		inputs := strings.Split(input, "/")
+		user = inputs[0]
+		input_1 = inputs[1]
+	} else {
+		pwd, _ := os.Getwd()
+		user = filepath.Base(pwd)
+		input_1 = input
+	}
+
+	if strings.Contains(input_1, ":") {
+		input_1s := strings.Split(input_1, ":")
+		repo = input_1s[0]
+		branch = input_1s[1]
+	} else {
+		repo = input_1
+		branch = "master"
+	}
+	fmt.Printf("%s/%s:%s\n", user, repo, branch)
+	codeload_uri := ""
+	if write {
+		codeload_uri = fmt.Sprintf("git clone --depth 1 git@github.com:%s/%s.git", user, repo)
+	} else {
+		codeload_uri = fmt.Sprintf("git clone --depth 1 git://github.com/%s/%s", user, repo)
+	}
+	cmd := exc.NewCMD(codeload_uri).Env("GOPATH").Cd("src/github.com/").Cd(user).Wd().Debug().Execute()
+	if install {
+		cmd.Cd(repo).Wd().Reset("go install").Execute()
+	}
+
+	fmt.Printf("cost time:%v\n", time.Now().Sub(start))
+}
